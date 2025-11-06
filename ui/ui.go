8ba6fa5e81b2eff36db27a65f402b5d8922a09e8
Alejandro Alemán Ramos <alejandro.aleman.ramos@gmail.com>
2025-11-06 07:00:59 +0100

    Added vim-like movements.

--- ui/ui.go
+++ ui/ui.go
@@ -8,39 +8,78 @@ import (
 )
 
 func GenerateTree(entry *dir.Entry) *tview.TreeView {
-	root := tview.NewTreeNode(entry.String()).SetColor(tcell.ColorRed)
-	tree := tview.NewTreeView().SetRoot(root).SetCurrentNode(root)
-
-	add(root, entry)
+	root := tview.NewTreeNode(entry.String()).
+		SetColor(tcell.ColorRed)
+	tree := tview.NewTreeView().
+		SetRoot(root).
+		SetCurrentNode(root)
 
 	// If a directory was selected, open it.
-	tree.SetSelectedFunc(func(node *tview.TreeNode) {
-		reference := node.GetReference()
-		if reference == nil {
-			return // Selecting the root node does nothing.
-		}
-		children := node.GetChildren()
-		if len(children) == 0 {
-			// Load and show files in this directory.
-			entry := reference.(*dir.Entry)
-			add(node, entry)
-		} else {
-			// Collapse if visible, expand if collapsed.
-			node.SetExpanded(!node.IsExpanded())
-		}
-	})
+	tree.SetSelectedFunc(setSelectedFunc)
+
+	tree.SetInputCapture(
+		func(event *tcell.EventKey) *tcell.EventKey {
+			if event.Key() == tcell.KeyRune {
+				switch event.Rune() {
+				case 'l':
+					node := tree.GetCurrentNode()
+					if node != nil {
+						reference := node.GetReference()
+						entry := reference.(*dir.Entry)
+
+						if entry.IsDir {
+							if len(entry.Files) != len(node.GetChildren()) {
+								add(node, entry)
+							}
+							node.Expand()
+						}
+					}
+					return nil
+				case 'h':
+					node := tree.GetCurrentNode()
+					if node != nil && node.IsExpanded() {
+						node.SetExpanded(false)
+					}
+
+					return nil
+				}
+			}
+			return event
+		})
+
+	add(root, entry)
 
 	return tree
 }
 
 func add(target *tview.TreeNode, entry *dir.Entry) {
 	target.SetReference(entry)
+
 	for _, file := range entry.Files {
-		node := tview.NewTreeNode(file.String()).SetReference(file.Path).SetSelectable(file.IsDir)
+		node := tview.NewTreeNode(file.String()).
+			SetReference(file.Path).
+			SetSelectable(true)
+			// SetSelectable(file.IsDir)
 		if file.IsDir {
 			node.SetColor(tcell.ColorGreen)
 		}
 		node.SetReference(&file)
 		target.AddChild(node)
+	}
+}
+
+func setSelectedFunc(node *tview.TreeNode) {
+	reference := node.GetReference()
+	if reference == nil {
+		return
+	}
+	children := node.GetChildren()
+	if len(children) == 0 {
+		// Load and show files in this directory.
+		entry := reference.(*dir.Entry)
+		add(node, entry)
+	} else {
+		// Collapse if visible, expand if collapsed.
+		node.SetExpanded(!node.IsExpanded())
 	}
 }
